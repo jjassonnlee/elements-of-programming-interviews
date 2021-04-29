93aad9e6a1bbefb4250cb8ef6882ef1e6d95c690
Jason Lee <jasoncs.lee@innovasolutions.com>
2021-04-29 21:22:29 +0800

    added dutch flag partition variant 1

--- arrays/dutch_flag_partition.py
+++ arrays/dutch_flag_partition.py
@@ -2,7 +2,6 @@
 Partition an array into three parts:
 Less than, Equal to, and Greater than a pivot value
 """
-
 def dutch_flag_partition(pivot_index, A):
     p = A[pivot_index]
     l, m, r = 0, 0, len(A)-1
@@ -18,9 +17,36 @@ def dutch_flag_partition(pivot_index, A):
             A[m], A[r] = A[r], A[m]
             r -= 1
 
-
 """
-Assuming that keys take one of three values, reorder the array 
+Assuming that keys take one of three values, reorder the array
 so that all objects with the same key appear together.
 """
 def variant1(A):
+    i = 0
+    j = 1
+    k = 0
+    # make i and j point and 2 diff values
+    while A[j] == A[i]:
+        j += 1
+    # move j to end of A. now i is at 0 and j is at len(A)-1
+    A[j], A[-1] = A[-1], A[j]
+    j = len(A) - 1
+    # use k to go thru A and swap with i or j as needed
+    while k <= j:
+        if A[k] == A[0]:
+            A[k], A[i] = A[i], A[k]
+            i += 1
+            k += 1
+        elif A[k] == A[-1]:
+            A[k], A[j] = A[j], A[k]
+            j -= 1
+        else:
+            k += 1
+
+
+"""
+Given an array A of n objects with keys that take one of four values,
+reorder the array such that all objects with the same key appear together.
+"""
+def variant2(A):
+
