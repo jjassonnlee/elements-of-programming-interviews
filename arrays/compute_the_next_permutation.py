27002226a370095432bf6850b97aaacd1aa101e6
Jason Lee <jasoncs.lee@innovasolutions.com>
2021-05-12 21:33:16 +0800

    update

---
+++ arrays/compute_the_next_permutation.py
@@ -0,0 +1,7 @@
+"""
+Given a permutation A, return the next permutation under dicitonary ordering.
+If input is [1,0,3,2], return [1,2,0,3]
+If input is [3,2,1,0], return []
+"""
+def next_permutation(A):
+    ...
